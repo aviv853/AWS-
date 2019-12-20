61222a3699d02dafde2a1899b7fd5f21977a6c30
Aviv Sheratzky <aviv312@gmail.com>
2019-12-20 19:21:55 +0000

    Added Twitter bootstrap to registration form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-    root to: 'pages#home'
-    get 'about', to: 'pages#about'
-    get 'pricing', to: 'pages#pricing'
-    resources :contacts, only: :create
-    get 'contact-us', to: 'contacts#new', as: 'new_contact'
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'pricing', to: 'pages#pricing'
+  resources :contacts, only: :create
+  get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
